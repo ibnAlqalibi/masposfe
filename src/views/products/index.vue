cd8580e5450533b9fc8003254410fa4d89d6895e
ibnAlqalibi <ibnuabdul969@gmail.com>
2025-05-26 09:27:48 +0700

    deleteButton component

--- src/views/products/index.vue
+++ src/views/products/index.vue
@@ -20,25 +20,7 @@
           <!-- Action Buttons -->
           <div class="absolute bottom-[14px] right-[14px] flex gap-1 sm:gap-2">
             <!-- Delete Button -->
-            <button
-              class="p-1 sm:p-1.5 bg-white/50 backdrop-blur-[30px] rounded-[4px] border border-white/50 hover:bg-gray-50"
-              @click="deleteData(product.id)"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-3 w-3 sm:h-4 sm:w-4 text-red-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-            </button>
+            <DeleteButton @delete="deleteData(product.id)" />
           </div>
         </div>
 
@@ -98,6 +80,7 @@ import { useCartStore } from "@/stores/cart.store.js";
 import CategoriesView from "@/views/categories/index.vue";
 import { PlusIcon } from "@heroicons/vue/24/solid";
 import ConfirmDeleteModal from "@/utils/components/deleteModal.vue";
+import DeleteButton from "@/utils/components/deleteButton.vue";
 
 export default {
   components: {
